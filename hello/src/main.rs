2d36f3ad0c4b860429525b2f2fe89e7037e296cd
FUJII Yoshitaka <yoshiyoshifujii@gmail.com>
2023-12-29 15:50:03 +0900

    edit main

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,24 @@
+use std::env;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
@@ -20,4 +39,30 @@ fn test_gcd() {
     assert_eq!(gcd(14, 15), 1);
 
     assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+}
+
+#[test]
+fn test_skip() {
+    let args = [1, 2, 3];
+
+    let mut it = args.iter().skip(0);
+    assert_eq!(it.next(), Some(&1));
+    assert_eq!(it.next(), Some(&2));
+    assert_eq!(it.next(), Some(&3));
+    assert_eq!(it.next(), None);
+
+    let mut it = args.iter().skip(1);
+    assert_eq!(it.next(), Some(&2));
+    assert_eq!(it.next(), Some(&3));
+    assert_eq!(it.next(), None);
+
+    let mut it = args.iter().skip(2);
+    assert_eq!(it.next(), Some(&3));
+    assert_eq!(it.next(), None);
+
+    let mut it = args.iter().skip(3);
+    assert_eq!(it.next(), None);
+
+    let mut it = args.iter().skip(4);
+    assert_eq!(it.next(), None);
 }
